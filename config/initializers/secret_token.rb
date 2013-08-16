bec74d0990be79b6f22b41ec9451851f2bee6cde
alex62wp7c <alex62wp7c@gmail.com>
2013-08-16 08:29:22 -0400

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deviseauth::Application.config.secret_token = 'cecd153719395182b210efd13de4f69e46186aa7f664068e61d3481f8ac7af39f7c915022752c5b1cfc3110e33d2c48333def85d1783f4730d4c50e88e8e7029'
